d244e45ba4313171c4596558af9f888d1760614b
Era James <51322608+era1112@users.noreply.github.com>
2024-10-26 20:31:26 -0400

    Update espc.h

--- src/espc.h
+++ src/espc.h
@@ -14,7 +14,7 @@
 //Weather Settings
 #define CITY_ID "6094817" //Ottawa https://openweathermap.org/current#cityid // Not needed, collides with lat/lon
 #define OPENWEATHERMAP_URL "http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&lang={lang}&units={units}&appid={apiKey}" //open weather api using lat lon
-#define OPENWEATHERMAP_APIKEY "fac78a4b0d75aa574766d966dc147263" //era1112
+#define OPENWEATHERMAP_APIKEY "xxx" //era1112
 #define TEMP_UNIT "metric" //metric = Celsius , imperial = Fahrenheit
 #define TEMP_LANG "en"
 #define WEATHER_UPDATE_INTERVAL 10 //must be greater than 5, measured in minutes
